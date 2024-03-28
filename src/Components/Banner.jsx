e09584db770ea62f0f8f6838622a3e5ffee3b3b4
protap1100 <protapb23@gmail.com>
2024-03-28 19:16:28 +0600

    final shot

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import image from '../../src/assets/images/BannerBook.png'
-
+ 
 const Banner = () => {
     return (
             <div className="hero py-32 bg-base-200 mt-5 rounded-3xl">
